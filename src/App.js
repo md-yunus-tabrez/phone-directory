a7d02725faf775cd48c72fe9580cef9a8b06f7ff
tabrez <myt0786@gmail.com>
2020-12-24 18:35:51 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
+import Header from "./Header"
 class App extends Component {
   render(){
+
   return (
+    
     <div>
+      <Header/>
       <button>Add</button>
       <div>
         <span>Name</span><br />
