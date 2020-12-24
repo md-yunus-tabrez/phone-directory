91b4928584a823e624cc1b132bb865948fefe5b2
tabrez <myt0786@gmail.com>
2020-12-24 18:27:32 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
-function App() {
+import React, {Component} from 'react';
+class App extends Component {
+  render(){
   return (
     <div>
-      {/* <div className="header">
-        phone directory
-    </div>
       <button>Add</button>
       <div>
         <span>Name</span><br />
         <span>Phone</span>
-      </div> */}
-      <label htmlFor="name">Name: </label>
-      <input id="name" type="text" placeholder="Type Here" defaultValue="Tabrez"/>
-    </div>
+      </div>
+    </div> 
   );
+  }
 }
 
 export default App;
