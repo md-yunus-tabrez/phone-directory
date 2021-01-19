480b829ad48b3dbda8b32f93547852956968656f
Tabrez <myt0786@gmail.com>
2021-01-20 00:51:08 +0530

    Added Delete Subscriber

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -17,9 +17,8 @@ class PhoneDirectory extends Component {
           id: 2,
           name: "rez",
           phone: "0987654321",
-        },
-      ],
-    };
+        }]
+    }
   }
 
   addSubscriberHandler = (newSubscriber) => {
@@ -31,13 +30,26 @@ class PhoneDirectory extends Component {
     }
     subscribersList.push(newSubscriber);
     this.setState({subscribersList: subscribersList })
-    
   }
+
+  deleteSubscriberHandler = (subscriberId) => {
+    let subscribersList = this.state.subscribersList;
+    let subscriberIndex = 0;
+    subscribersList.forEach(function (subscriber, index){
+      if(subscriber.id === subscriberId){
+        subscriberIndex = index;
+      }
+    }, this);
+    let newSubscribers = subscribersList;
+    newSubscribers.splice(subscriberIndex, 1);
+    this.setState({subscribers: newSubscribers});
+  }
+
   render() {
     return (
       <Router>
         <div className="main-container">
-          <Route exact path="/" render={(props) => (<ShowSubscribers {...props} subscribersList={this.state.subscribersList} /> )} />
+          <Route exact path="/" render={(props) => (<ShowSubscribers {...props} subscribersList={this.state.subscribersList} deleteSubscriberHandler={this.deleteSubscriberHandler}/> )} />
           <Route exact path="/add" render={({history}, props) => (<AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} /> )} />
         </div>
       </Router>
--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -4,6 +4,10 @@ import "./ShowSubscribers.css";
 import { Link } from "react-router-dom";
 class ShowSubscribers extends Component {
 
+  onDeletedClick = (subscriberId) => {
+    this.props.deleteSubscriberHandler(subscriberId);
+  }
+
   render() {
  
     return (
@@ -22,9 +26,8 @@ class ShowSubscribers extends Component {
               <div key={sub.id} className="grid-container ">
                 <span className="grid-item ">{sub.name}</span>
                 <span className="grid-item ">{sub.phone}</span>
-
                 <span className="grid-item action-btn-container">
-                  <button className="custom-btn delete-btn">Delete</button>
+                  <button className="custom-btn delete-btn" onClick={this.onDeletedClick.bind(this,sub.id)}>Delete</button>
                 </span>
               </div>
             );
